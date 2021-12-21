9ee9f90880a0b98b59b43e68e63cad729e217105
LokotamaTheMastermind <57059138+LokotamaTheMastermind@users.noreply.github.com>
2021-12-21 16:56:56 +0100

    clean up src/App.vue & re-organized navigation menu with styles

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <nav><router-link to="/">Home</router-link></nav>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/demo">Demo</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
   <router-view />
 </template>
 
 <style lang="scss">
-main {
+nav {
   padding: 30px;
 
   a {
