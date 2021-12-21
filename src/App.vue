7eba0ff2663bac9db09942a78bc32c36fc31a1dd
LokotamaTheMastermind <57059138+LokotamaTheMastermind@users.noreply.github.com>
2021-12-21 12:44:23 +0100

    working on homepage

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
-  <nav>
+  <main>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+
+    <img src="./assets/vuejs.svg" alt="Vue.js Logo" />
+    <img src="./assets/tailwindcss.svg" alt="Tailwind CSS Logo" />
+  </main>
   <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
+main {
   padding: 30px;
 
   a {
