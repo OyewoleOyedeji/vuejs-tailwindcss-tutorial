2c397c49e5143ba24a38352f858233482472613a
LokotamaTheMastermind <57059138+LokotamaTheMastermind@users.noreply.github.com>
2021-12-21 16:56:06 +0100

    added titleMixin to createApp function

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/tailwind.css";
+import titleMixin from "./mixins/titleMixin";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).mixin(titleMixin).mount("#app");
